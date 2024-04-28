0e5e6f9e6d6c3f97995d113912d6844938d2416d
Madeline Leano <mleano@mit.edu>
2024-04-27 21:09:53 -0400

    my work

--- src/routes/vis4/+page.svelte
+++ src/routes/vis4/+page.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
+  let currentImage = ""; // Store the path of the current image
+
+  // Function to set the current image when a button is clicked
+  function setImage(imagePath: string) {
+    currentImage = imagePath;
+  }
 </script>
 
-<h1>Vis 4</h1>
-<p>Information</p>
+<p>
+  Select a race to see the percentage of that group per neighborhood in Boston
+  against the percentage of evictions in that neighborhood in Boston.
+</p>
+
+<!-- Buttons to select different images -->
+<button on:click={() => setImage("white.png")}>White</button>
+<button on:click={() => setImage("asian.png")}>Asian</button>
+<button on:click={() => setImage("hispanic.png")}>Hispanic</button>
+<button on:click={() => setImage("africanamerican.png")}
+  >African American</button
+>
+
+<!-- Display the selected image -->
+{#if currentImage}
+  <img src={currentImage} alt="Selected Image" width="200" height="200" />
+{/if}

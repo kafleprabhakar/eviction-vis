00548fabd4da071689a2114e7e7f8917706ee7fb
Prabhakar Kafle <pkafleprabhakar@gmail.com>
2024-05-12 11:44:57 -0400

    fixed error on credits

--- src/lib/components/Credits.svelte
+++ src/lib/components/Credits.svelte
@@ -10,10 +10,10 @@
 
     <div class='references'>
         <h3 class='credit-header'>References</h3>
-        <p class='credit-words'><a class='credit-link' target='_blank href='https://www.kaggle.com/datasets/yingzhou474/boston-neighborhoods-geojson'>Boston Neighborhoods</a></p>
-        <p class='credit-words'><a class='credit-link' target='_blank href='https://www.bostonevictions.org/data'>Boston Evictions</a></p>
-        <p class='credit-words'><a class='credit-link' target='_blank href='https://www.urbandisplacement.org/maps/housing-precarity-risk-model/'>Eviction Lab</a></p>
-        <p class='credit-words'><a class='credit-link' target='_blank href='https://justshelter.org/stories/#stories-image-1'>Testimonials</a></p>
+        <p class='credit-words'><a class='credit-link' target='_blank' href='https://www.kaggle.com/datasets/yingzhou474/boston-neighborhoods-geojson'>Boston Neighborhoods</a></p>
+        <p class='credit-words'><a class='credit-link' target='_blank' href='https://www.bostonevictions.org/data'>Boston Evictions</a></p>
+        <p class='credit-words'><a class='credit-link' target='_blank' href='https://www.urbandisplacement.org/maps/housing-precarity-risk-model/'>Eviction Lab</a></p>
+        <p class='credit-words'><a class='credit-link' target='_blank' href='https://justshelter.org/stories/#stories-image-1'>Testimonials</a></p>
 
         <p class='credit-words'>Placeholder</p>
         <p class='credit-words'>Placeholder</p>
